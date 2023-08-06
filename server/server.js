3e8c7b75c392fb52b28c60c0ff3e2a9d4b2ccc19
mdazad1508 <mdazad15081999@gmail.com>
2023-08-06 12:38:32 +0530

    api key

--- server/server.js
+++ server/server.js
@@ -9,8 +9,7 @@ app.use(express.json());
 app.use(cors());
 const port = 8000;
 
-const leap = new Leap(process.env.LEAP_API_KEY);
-console.log(process.env.LEAP_API_KEY);
+const leap = new Leap("e7147dda-1eab-4abf-be98-fc6b0d215657");
 
 
 
